069c4de538015dfc4684ecb3cc4780efd9ada6e2
Jan d'Orgeville <jdorgeville@gmail.com>
2021-01-05 12:24:40 -0500

    some ajustment

--- README.md
+++ README.md
@@ -1,8 +1,14 @@
 # git bootstrap
 
-Nothing more than a simple git clone
+`'bootstrap.sh'` is nothing more than a script to make a `'git clone'` to `'/opt'` folder
 
 ```bash
 curl -s https://raw.githubusercontent.com/chtimi59/gitbootstrap/master/bootstrap.sh | bash -s <repo_name> [ssh_keypath]
 ```
 
+If `ssh_keypath` is provided, then a symlink is created such as:
+
+```bash
+drwx------ root root /opt/<repo_name>/.ssh/
+lrwxrwxrwx root root /opt/<repo_name>/.ssh/github -> $ssh_keypath
+```
--- bootstrap.sh
+++ bootstrap.sh
@@ -7,39 +7,34 @@ if [ -z $1 ]; then
 fi
 
 GIT_REPO_NAME=$1
-GIT_REPO_URL=git@github.com:chtimi59/$GIT_REPO_NAME.git
-GIT_FOLDER=/opt/$GIT_REPO_NAME
 GIT_SSHKEY=$2
 
-if [ ! -z $GIT_SSHKEY ]; then
-    GIT_SSH_COMMAND="ssh -i $GIT_SSHKEY -o IdentitiesOnly=yes"
-fi
+GIT_REPO_URL=git@github.com:chtimi59/$GIT_REPO_NAME.git
+GIT_FOLDER=/opt/$GIT_REPO_NAME
 
-# need git, at least
-which git 1>/dev/null || sudo apt install git
-
-# git clone
 if [ -e $GIT_FOLDER ]; then
-    echo "warning: already installed, try updating"
-    cd $GIT_FOLDER 
-    if [ ! -z $GIT_SSHKEY ]; then
-        sudo GIT_SSH_COMMAND="$GIT_SSH_COMMAND" git pull
-    else
-        sudo git pull
-    fi
-else
-    if [ ! -z $GIT_SSHKEY ]; then
-        sudo GIT_SSH_COMMAND="$GIT_SSH_COMMAND" git clone $GIT_REPO_URL $GIT_FOLDER
-    else
-        git clone $GIT_REPO_URL $GIT_FOLDER
-    fi
+    echo "$GIT_FOLDER already exists"
+    exit 0
 fi
 
-# add a link to used private key
+# need git, at least!
+which git 1>/dev/null || (
+    set +e
+    sudo apt install git
+    set -e
+)
+
+# actual git clone
 if [ ! -z $GIT_SSHKEY ]; then
+    sudo GIT_SSH_COMMAND="ssh -i $GIT_SSHKEY -o IdentitiesOnly=yes" git clone $GIT_REPO_URL $GIT_FOLDER
+    
+    # add a symlink to used private key
     sudo mkdir -p $GIT_FOLDER/.ssh/
-    sudo chmod u+xr,go-rwx $GIT_FOLDER/.ssh
+    sudo chmod u+xr,go-rwx $GIT_FOLDER/.ssh # give it some privacy
     sudo ln -f -s $GIT_SSHKEY $GIT_FOLDER/.ssh/github
+
+else
+    sudo git clone $GIT_REPO_URL $GIT_FOLDER
 fi
 
 # shortcut
