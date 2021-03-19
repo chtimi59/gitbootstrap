ff10b447945cf210fc38903a26e1844e19224f70
Jan d'Orgeville <jdorgeville@gmail.com>
2021-03-19 12:02:04 -0400

    remove sudo

--- bootstrap.sh
+++ bootstrap.sh
@@ -12,10 +12,14 @@ GIT_SSHKEY=$2
 GIT_REPO_URL=git@github.com:chtimi59/$GIT_REPO_NAME.git
 GIT_FOLDER=/opt/$GIT_REPO_NAME
 
+# create folder
 if [ -e $GIT_FOLDER ]; then
     echo "$GIT_FOLDER already exists"
     exit 0
 fi
+sudo mkdir $GIT_FOLDER
+sudo chown $USER $GIT_FOLDER
+sudo chgrp $USER $GIT_FOLDER
 
 # need git, at least!
 which git 1>/dev/null || (
@@ -26,15 +30,14 @@ which git 1>/dev/null || (
 
 # actual git clone
 if [ ! -z $GIT_SSHKEY ]; then
-    sudo GIT_SSH_COMMAND="ssh -i $GIT_SSHKEY -o IdentitiesOnly=yes" git clone $GIT_REPO_URL $GIT_FOLDER
-    
-    # add a symlink to used private key
-    sudo mkdir -p $GIT_FOLDER/.ssh/
-    sudo chmod u+xr,go-rwx $GIT_FOLDER/.ssh # give it some privacy
-    sudo ln -f -s $GIT_SSHKEY $GIT_FOLDER/.ssh/github
+    GIT_SSH_COMMAND="ssh -i $GIT_SSHKEY -o IdentitiesOnly=yes" git clone $GIT_REPO_URL $GIT_FOLDER
 
+    # add a symlink to used private key
+    mkdir $GIT_FOLDER/.ssh
+    chmod u+xr,go-rwx $GIT_FOLDER/.ssh # give it some privacy
+    ln -f -s $GIT_SSHKEY $GIT_FOLDER/.ssh/github
 else
-    sudo git clone $GIT_REPO_URL $GIT_FOLDER
+    git clone $GIT_REPO_URL $GIT_FOLDER
 fi
 
 # shortcut
